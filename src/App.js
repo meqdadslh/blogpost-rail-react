625ec24a259184e5fdd8e769407f73e4005df07b
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-06-07 17:38:58 -0400

    frontEnd updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// Import All Our Components
+import AllPosts from "./pages/AllPosts";
+import SinglePost from "./pages/SinglePost";
+import Form from "./pages/Form";
 
-function App() {
+// Import React and hooks
+import React, { useState, useEffect } from "react";
+
+// Import components from React Router
+import { Route, Switch } from "react-router-dom";
+
+
+function App(props) {
+  ////////////////////
+  // Style Objects
+  ////////////////////
+
+  const h1 = {
+    textAlign: "center",
+    margin: "10px",
+  };
+
+  ///////////////
+  // State & Other Variables
+  ///////////////
+
+
+  // Our Api Url from backend Heroku
+  const url = "https://blog-rail-ma.herokuapp.com/posts/";
+
+    // State to Hold The List of Posts
+    const [posts, setPosts] = useState([]);
+
+    // Function to get list of Todos from API
+    const getTitles = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    //////////////
+    // useEffects
+    //////////////
+
+    // useEffect to get list of titles when page loads
+  useEffect(() => {
+    getTitles();
+  }, []);
+
+  
+  /////////////////////
+  // returned JSX
+  /////////////////////
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={h1}>Titles and Roles</h1>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => <AllPosts {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/post/:id"
+          render={(routerProps) => (
+            <SinglePost {...routerProps} posts={posts} />
+          )}
+        />
+        <Route
+          path="/new"
+          render={(routerProps) => <Form {...routerProps} />}
+        />
+        <Route
+          path="/edit"
+          render={(routerProps) => <Form {...routerProps} />}
+        />
+      </Switch>
     </div>
   );
 }
